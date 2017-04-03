5e2a4829016029d8d7c5476f768b9beb352face1
MJacobs92 <MJacobs92@email>
2017-04-02 21:20:52 -0400

    added code to signup-submit to insert singles into the database

--- db_connection.php
+++ db_connection.php
@@ -1,12 +1,9 @@
 <?php                                                                           
   require_once('credentials.php');                                              
-                                                                                   
-  function connect() {                                                          
+                                                                                                                                            
     $conn = mysqli_connect(SERVER_URL, USER, PASSWORD, DB_NAME);            
     if(mysqli_connect_errno()) {                                                   
       $err_msg = "Connection failed: ".mysqli_connect_error();                                                                  
       exit($err_msg);                                                                  
     }                                                                              
-    return $conn;                                                            
-  }   
 ?>
--- signup-submit.php
+++ signup-submit.php
@@ -1,4 +1,6 @@
-<?php include("top.html"); ?>
+<?php include("top.html");
+	  include("db_connection.php");
+?>
 
 <!-- Take form input from signup.php and write it to the singles.txt file -->
 <?php
@@ -40,13 +42,35 @@ function printError($errorString){
 
 //make sure the first letter in each word in the name string capitalized
 $userName = ucwords($_POST["name"]);
-$userData = $userName;
-foreach ($_POST as $key => $value) {
-	if ($key != "name"){
-		$userData = $userData.",".$value;
-	}
-}
-file_put_contents("singles.txt", "\n".$userData, FILE_APPEND);
+
+$insertionSQL = "INSERT INTO singles (name, gender, age) VALUES (""'".$userName."','"._POST["gender"]."','"._POST["age"]."'";
+
+mysqli_query($conn,insertionSQL);
+
+$single_id = mysqli_insert_id($conn);
+
+insertionSQL = "INSERT INTO favorite_os (single_id, os) VALUES (""'".$single_id."','"._POST["OS"]."'";
+
+mysqli_query($conn,insertionSQL);
+
+insertionSQL = "INSERT INTO personality_types (single_id, type) VALUES (""'".$single_id."','"._POST["persona"]."'";
+
+mysqli_query($conn,insertionSQL);
+
+insertionSQL = "INSERT INTO seeking_age (single_id, min_age, max_age) VALUES (""'".$single_id."','"._POST["minage"]."','"._POST["maxage"]."'";
+
+mysqli_query($conn,insertionSQL);
+
+mysqli_close($conn);
+
+
+// $userData = $userName;
+// foreach ($_POST as $key => $value) {
+// 	if ($key != "name"){
+// 		$userData = $userData.",".$value;
+// 	}
+// }
+// file_put_contents("singles.txt", "\n".$userData, FILE_APPEND);
 ?>
 
 <!-- <div>
