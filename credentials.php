1ea94fcf29ae2881d00fb61f56b2a2a634d0eb95
MJacobs92 <MJacobs92@email>
2017-04-02 20:57:00 -0400

    implemented code that gets database connection to be used for queries

---
+++ credentials.php
@@ -0,0 +1,6 @@
+<?php                                                                           
+  define("SERVER_URL", "localhost");                                             
+  define("USER", "root");                                                    
+  define("PASSWORD", "password");                                                
+  define("DB_NAME", "nerdluv");                                                    
+?>  
