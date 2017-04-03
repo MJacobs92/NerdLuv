0485f8477b339bbc5c9f466ac4b9db6981740573
MJacobs92 <MJacobs92@email>
2017-04-02 22:41:21 -0400

    added logic to query current user and get all info from all tables about that user

--- matches-submit.php
+++ matches-submit.php
@@ -1,20 +1,57 @@
-<?php include("top.html"); ?>
+<?php include("top.html"); 
+      include("db_connection.php");
+
+	  error_reporting(E_ALL);
+	  ini_set('display_errors', 1);
+?>
 
 <?php
 
 //Variables
-$singlesFile = file_get_contents("singles.txt");
-$singles = explode("\n", $singlesFile);
-$notInFile = false;
+// $singlesFile = file_get_contents("singles.txt");
+// $singles = explode("\n", $singlesFile);
+// $notInFile = false;
 //Get User Information
 $userName = $_GET["name"];
-$userInfo = array();
-foreach ($singles as $people) {
-	$userInfo = explode(",", $people);
-	if($userInfo[0] == $userName){   
-		break;
-	}
-}
+
+$sqlQuery = "SELECT * FROM singles where name = '" . $userName . "';";
+$returned_singles = mysqli_query($conn, $sqlQuery);
+
+while ($row = $returned_singles->fetch_assoc()) {
+        $current_single_id = $row["id"];
+        $current_single_gender = $row["gender"];
+        $current_single_age = (int)$row["age"];
+        
+        $sqlQuery = "SELECT name FROM personality_type WHERE single_id = $current_single_id.";"";
+  
+        $returned_personality_type = mysqli_query($conn, $sqlQuery);
+
+        $current_single_personality = $returned_personality_type->fetch_assoc()["name"];
+
+        $sqlQuery = "SELECT name FROM fav_os WHERE single_id = $current_single_id . ";"";
+        
+        $returned_favorite_os = mysqli_query($conn, $sqlQuery);
+        $current_single_os = $returned_favorite_os->fetch_assoc()["name"];
+
+
+        /* get users min and max seek age */
+        $sqlQuery = "SELECT min_age, max_age FROM seeking_age WHERE single_id = $current_single_id . ";"";
+       
+        $returned_seeking_age = mysqli_query($conn, $sqlQuery);
+        $current_single_seeking_ages = $response_seek_ages->fetch_assoc();
+        $current_single_min_age = (int)$current_single_seeking_ages["min_age"];
+        $current_single_max_age = (int)$current_single_seeking_ages["max_age"];
+    }
+
+$potential_matches = array();
+
+
+// foreach ($singles as $people) {
+// 	$userInfo = explode(",", $people);
+// 	if($userInfo[0] == $userName){   
+// 		break;
+// 	}
+// }
 
 //show all matches
 function showMatches(){
